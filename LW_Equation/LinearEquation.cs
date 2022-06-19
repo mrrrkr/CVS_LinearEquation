e7d35ae1d07407db574e0dc11ffe815935c7b2cd
AVAtarMod <respawner289@gmail.com>
2022-06-19 16:42:10 +0300

    Add comments to constructor, operator+, operator-

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -10,6 +10,13 @@ namespace LW_Equation
     {
         List<float> coefficients;
         public int Size => coefficients.Count;
+
+        /// <summary>
+        /// Конструирует уравнение вида coefficients[0]x + ... + coefficients[N-2]y + (aN)z + b = 0
+        /// </summary>
+        /// <param name="b">Свободный член</param>
+        /// <param name="aN">Последний коэффициент</param>
+        /// <param name="coefficients">Остальные коэффициенты</param>
         public LinearEquation(float b, float aN, params float[] coefficients)
         {
             this.coefficients = new List<float>();
@@ -22,13 +29,20 @@ namespace LW_Equation
             this.coefficients = new List<float>();
             this.coefficients = coefficients;
         }
-        static public LinearEquation operator +(LinearEquation first, float second)
+
+        /// <summary>
+        /// Суммирует свободный член first с second
+        /// </summary>
+        static public LinearEquation operator+ (LinearEquation first, float second)
         {
             LinearEquation equation = first;
             equation.coefficients[0] *= second;
             return equation;
         }
-        static public LinearEquation operator -(LinearEquation first, float second)
+        /// <summary>
+        /// Вычитает second из свободного члена first
+        /// </summary>
+        static public LinearEquation operator- (LinearEquation first, float second)
         {
             LinearEquation equation = first;
             equation.coefficients[0] /= second;
